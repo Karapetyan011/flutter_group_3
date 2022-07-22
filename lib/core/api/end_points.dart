79a89cc25e330e5abc2d21af75c977eb6e17a7e9
Davo <098888837@mail.ru>
2022-07-22 19:21:13 +0400

    Merge remote-tracking branch 'origin/main' into davo
    have been created star_wars_page_from_starships
    have been changed end_points.dart
    have been changed swservice.dart

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -4,6 +4,6 @@ abstract class EndPoint {
   static const person = 'api/people/1/';
   static const planets = 'api/planets/5/';
   static const species = 'api/species';
-  static const starships = 'api/starships';
+  static const starships = 'api/starships/9/';
   static const vehicles = 'api/vehicles';
 }
