023b78a3229c42db5a3cb9eb12b35b1255700b99
hrantkarapetyan011 <hrantkarapetyan011@gmail.com>
2022-07-27 21:16:48 +0400

    have been created grid view, navigator, dice game screens

---
+++ lib/screens/grid_view_screen/grid_view_count/grid_item_component.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class GridItemComponent {
+  const GridItemComponent({required this.title, required this.icon});
+
+  final String title;
+  final IconData icon;
+}
