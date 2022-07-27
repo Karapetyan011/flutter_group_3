da3024277831921d6232cf4bd26a5a54c032b71a
hrantkarapetyan011 <hrantkarapetyan011@gmail.com>
2022-07-27 21:16:48 +0400

    have been created grid view, navigator, dice game screens

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,22 +1,3 @@
 import 'package:flutter/material.dart';
 
-import '../screens/grid_view_screen/grid_view_count/grid_item_component.dart';
-
-const List<GridItemComponent> myCustomList = <GridItemComponent>[
-  GridItemComponent(title: 'Home', icon: Icons.home),
-  GridItemComponent(title: 'Contact', icon: Icons.contacts),
-  GridItemComponent(title: 'Map', icon: Icons.map),
-  GridItemComponent(title: 'Phone', icon: Icons.phone),
-  GridItemComponent(title: 'Camera', icon: Icons.camera_alt),
-  GridItemComponent(title: 'Setting', icon: Icons.settings),
-  GridItemComponent(title: 'Album', icon: Icons.photo_album),
-  GridItemComponent(title: 'WiFi', icon: Icons.wifi),
-];
-
 const kBlue = Color(0xff1d9bf0);
-
-
-// Email & Password Patterns
-const String kEmailPattern = r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]';
-const String kPasswordPattern =
-    r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
