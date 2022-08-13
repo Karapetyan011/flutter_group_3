2c4d9407ed16c453753790855806622ce1e8882b
hrantkarapetyan011 <hrantkarapetyan011@gmail.com>
2022-08-13 13:09:56 +0400

    has been created to do list with provider

---
+++ lib/to_do/models/task_provider.dart
@@ -0,0 +1,36 @@
+///Created by HrAnT
+
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test_group_2/to_do/models/task.dart';
+
+class TaskProvider extends ChangeNotifier {
+  final List<Task> _tasks = [
+    Task(name: 'My To Do Example'),
+  ];
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
